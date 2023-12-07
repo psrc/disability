ee803fcf8bb01f3478a34472348bc145a33f7475
cyyllam <cyyllam@gmail.com>
2023-12-07 10:47:42 -0800

    updated functions with correct filters

--- qc-labor-force.R
+++ qc-labor-force.R
@@ -1,6 +1,11 @@
 # QC labor force with disability numbers
+# To run this script successfully, install github version of tidycensus: devtools::install_github("walkerke/tidycensus")
+
 library(psrccensus)
+library(tidycensus)
 library(tidyverse)
+library(magrittr)
+
 
 ## 2022 ACS 1-year B18120 Employment Status by Disability Status and Type (Civilian Non-institutionalized Population 18 to 64 Years) ----
 
@@ -18,8 +23,14 @@ pums <- get_psrc_pums(span = span,                                            # 
                       dyear = dyear,                                          # Last data year of span
                       level = "p",                                            # Unit of analysis == household ("p" used for person)
                       vars = c("STANDARD_JOBSECTOR",                          # Cov Emp Sectors
-                               "DIS"                                          # Disability recode
+                               "DIS",                                         # Disability recode
+                               "ESR",                                         # Employment status recode
+                               "AGEP"                                         # Age
                       ))   
+
+# filter for Civilian/Armed persons and between 18-64 years
+pums <- pums %>% filter((grepl("^(Civilian|Armed) ", as.character(ESR)) & between(AGEP, 18,64)))
+# pums <- pums %>% filter(ESR %in% c(1,2,4,5) & between(AGEP, 18,64))
 
 df <- psrc_pums_count(pums, 
                       group_vars = c("STANDARD_JOBSECTOR",       
@@ -27,14 +38,15 @@ df <- psrc_pums_count(pums,
                       )) %>% 
   mutate(acs_type = paste0('acs', span))
 
-if(dyear == 2022) {
-  df <- df %>% 
-    mutate(DIS = as.character(DIS)) %>% 
-    mutate(DIS = case_match(DIS,
-                            "1" ~ "With a disability",
-                            "2" ~ "Without a disability",
-                            .default = DIS))
-}
+## uncomment this line if using CRAN tidycensus
+# if(dyear == 2022) {
+#   df <- df %>% 
+#     mutate(DIS = as.character(DIS)) %>% 
+#     mutate(DIS = case_match(DIS,
+#                             "1" ~ "With a disability",
+#                             "2" ~ "Without a disability",
+#                             .default = DIS))
+# }
 
 denom <- df %>% 
   filter(STANDARD_JOBSECTOR == 'Total') %>%
@@ -50,8 +62,8 @@ d <- df_sum %>%
   group_by(acs_type, DATA_YEAR, COUNTY, STANDARD_JOBSECTOR, DIS) %>%
   summarise(count, count_moe, cv, share = count/count_denom, share_moe = moe_prop(count, count_denom, count_moe, moe_denom))
 
-
-d_with_disab <- d %>% 
+# Check, compare to employed in labor force est
+d %>% 
   filter(DIS == 'With a disability' & !is.na(STANDARD_JOBSECTOR)) %>% 
   ungroup() %>% 
   summarise(count = sum(count))
